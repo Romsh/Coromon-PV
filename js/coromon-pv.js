25b4a6a1b6370d16b74c365135549baebceaa730
Romsh Kirtha <samuel.umek@gmail.com>
2021-03-21 20:44:07 +0100

    Optimisation du système de thème

--- js/coromon-pv.js
+++ js/coromon-pv.js
@@ -1,41 +1,24 @@
-﻿$(document).ready(function(){
-    $(".theme-light").on("click",function(){
-        $(".body-light").toggleClass("body-dark");
-        $(".body-dark").toggleClass("body-light");
-    
-        $(".app-title-light").toggleClass("app-title-dark");
-        $(".app-title-dark").toggleClass("app-title-light");
-    
-        $(".navbox-light").toggleClass("navbox-dark");
-        $(".navbox-dark").toggleClass("navbox-light");
-    
-        $(".box-light").toggleClass("box-dark");
-        $(".box-dark").toggleClass("box-light");
-    
-        $(".result-light").toggleClass("result-dark");
-        $(".result-dark").toggleClass("result-light");
-    
-        $(".theme-light").toggle();
-        $(".theme-dark").toggle();
-    });
-    
-    $(".theme-dark").on("click",function(){
-        $(".body-dark").toggleClass("body-light");
-        $(".body-light").toggleClass("body-dark");
-    
-        $(".app-title-dark").toggleClass("app-title-light");
-        $(".app-title-light").toggleClass("app-title-dark");
-    
-        $(".navbox-dark").toggleClass("navbox-light");
-        $(".navbox-light").toggleClass("navbox-dark");
-    
-        $(".box-dark").toggleClass("box-light");
-        $(".box-light").toggleClass("box-dark");
-    
-        $(".result-dark").toggleClass("result-light");
-        $(".result-light").toggleClass("result-dark");
-    
-        $(".theme-light").toggle();
-        $(".theme-dark").toggle();
-    });
+﻿var theme = sessionStorage.getItem('theme');
+
+if(theme === null){
+    theme = 'light';
+    sessionStorage.setItem('theme','light');
+}else if(theme === 'dark'){
+    $('link[href="css/coromon-pv-light.css"]').attr('href','css/coromon-pv-dark.css');
+}
+
+
+$(document).ready(function(){
+    $("#theme-btn").on('click',function(){     
+        if(theme === 'dark'){
+            $('link[href="css/coromon-pv-dark.css"]').attr('href','css/coromon-pv-light.css');
+            theme = 'light';
+            sessionStorage.setItem('theme','light');
+
+        }else if(theme === 'light'){
+            $('link[href="css/coromon-pv-light.css"]').attr('href','css/coromon-pv-dark.css');
+            theme = 'dark';
+            sessionStorage.setItem('theme','dark');
+        }
+    })
 });
